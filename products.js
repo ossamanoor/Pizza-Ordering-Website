d61cb011fc674ab96a7e8f5af05666619d517c84
Osama Noor <88425627+ossamanoor@users.noreply.github.com>
2022-08-15 14:04:03 +0500

    Pizza Ordering Website

---
+++ products.js
@@ -0,0 +1,78 @@
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var products = {
+  1: {
+    
+    name : "Mashroom Pizza",
+    desc : ".....",
+    img : "s-img-1.png",
+    price : 1300
+  },
+  2: {
+    name : "Pizza World Special",
+    desc : "......",
+    img : "s-img-2.jpg",
+    price : 1200
+  },
+  3: {
+    name : "Classic Sausage Pizza",
+    desc : "......",
+    img : "s-img-3.png",
+    price : 1500
+  },
+  4: {
+    name : "Cheese Brust",
+    desc : "....... ",
+    img : "s-img-4.png",
+    price : 1100
+  },
+  5: {
+    name : "Vegitable Pizza",
+    desc : "....... ",
+    img : "s-img-5.png",
+    price : 1000
+  },
+  9: {
+    name : "Mixed Pizza",
+    desc : "....... ",
+    img : "s-img-6.png",
+    price :1200
+  },
+
+  6: {
+    name : "Fries",
+    desc : "....... ",
+    img : "p-6.jpg",
+    price : 100
+  },
+  7: {
+    name : "Mint Margrita",
+    desc : "....... ",
+    img : "p-22.png",
+    price : 150
+  },
+  8: {
+    name : "Steak Fries",
+    desc : "....... ",
+    img : "p-5.jpg",
+    price : 600
+  },
+  9: {
+    name : "Cold Drink",
+    desc : "....... ",
+    img : "p-4.png",
+    price : 150
+  },
+  10: {
+    name : "Deal 1",
+    desc : "....... ",
+    img : "p-7.png",
+    price : 2100
+  },
+
+
+
+
+
+
+
+};
